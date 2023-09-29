1bf0636a7a547d83f56dbf608f80685e76f6b52e
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-29 14:48:15 -0400

    Initial Commit

---
+++ FrameworksGuide/FrameworksGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrameworksGuideApp.swift
+//  FrameworksGuide
+//
+//  Created by Jordan Haynes on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct FrameworksGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
